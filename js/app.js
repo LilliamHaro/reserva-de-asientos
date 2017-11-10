f311612cc5e5c5a63add94c434762e890052c0ed
LilliamHaro <qualia950@gmail.com>
2017-11-10 10:54:10 -0500

    subiendo avance con js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Declarar un arreglo que representara los asientos de nuestro
+//  asiento vacío = false ------ ocupado = true
 var airlineSeats = [
   false,
   false,
@@ -8,25 +10,71 @@ var airlineSeats = [
   false,
   false,
   false,
-  false,
-]
-//contador para guiarnos para hallar numeros de asientos ocupados
-var busySeats = 0;
+  false
+];
 
+// Contador que nos ayudara a rastrear el numero de asientos acupados
+var busySeats = 0;
+// mostrar los asientos
 var paintSeats = function(array) {
   var containerSeats = document.getElementById('seats');
-
+  // pasar por el arreglo
   for (var i = 0; i < array.length; i++) {
     var seat = document.createElement('div');
     seat.className = 'seats';
 
-    if(1 < 4) {
+    // del primer asiento al cuatro 1 - 4 --> en nuestro arreglo va a ser Primera clase, que seria del indice 0 al 3
+    if (i < 4) {
       seat.style.background = 'purple';
     } else {
       seat.style.background = 'yellow';
     }
     containerSeats.appendChild(seat);
   }
+};
+// reservar de asientos
+var reserve = function() {
+  var btn = document.getElementById('btn');
+  btn.addEventListener('click', chooseZone);
+};
+
+var chooseZone = function() {
+  var choice = prompt('En que zona prefieres reservar \n 1. Primera Clase \n 2. Economica \n \n Por favor ingresa el numero de tu preferencia');
+
+  if (choice === "1") {
+    checkFirstClassZone();
+  } else if (choice === "2") {
+      checkEconomicZone();
+  } else {
+      alert('Por favor ingresa un numero valido');
+  }
+};
+
+var checkFirstClassZone = function() {
+    var zone = 'Primera Clase';
+    // recorre ele elemnto del 0 al 3 y verifica cuales estan disponles
+    for (var index = 0; index < 4; index++) {
+      if(airlineSeats[index]===false) {
+        airlineSeats[index] = true;
+        // al reservar un asiento ya no debemos seguir recoriendo el elemento
+        //recorremos el for con un reak
+        break;
+      }
+    }
+};
+
+var checkEconomicZone = function() {
+  var zone = 'Economica';
+  // recorre ele elemnto del 0 al 3 y verifica cuales estan disponles
+  for (var index = 4; index < 10; index++) {
+    if(airlineSeats[index]===false) {
+      airlineSeats[index] = true;
+      // al reservar un asiento ya no debemos seguir recoriendo el elemento
+      //recorremos el for con un reak
+      break;
+    }
+  }
 
 }
 paintSeats(airlineSeats);
+reserve();
